963a1ec8eaad4aecafbb1ca4e917e34de1b88827
Justin Lung <justin.lung@hva.nl>
2021-07-05 12:29:46 +0200

    cahnges made

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -8,7 +8,7 @@
     <label for="repeatedPassword">Repeat password</label>
     <input type="password" name="repeatedPassword" v-model="repeatedPassword" />
     <label for="image">Profile Picture (Not required)</label>
-    <input type="file" name="image" ref="file" @change="onFileChange">
+    <input type="file" name="image" ref="file" @change="onFileChange" />
     <button @click="checkForm">Submit</button>
     <p v-if="errors.length">Please correct the following error(s)</p>
     <div class="errors" v-for="error in errors" :key="error">
@@ -32,48 +32,53 @@ export default {
   methods: {
     checkForm() {
       this.errors = [];
-        //Check if username is empty
-      if (this.username == "") 
-      this.errors.push("Fill in your username");
+      //Check if username is empty
+      if (this.username == "") this.errors.push("Fill in your username");
 
-        //Check if username is under 20 characters
+      //Check if username is under 20 characters
       if (this.username.length > 20)
         this.errors.push("Username must be 20 characters or fewer");
 
-        //Check if password is empty
-      if (this.password == "") 
-      this.errors.push("Fill in your password");
+      //Check if password is empty
+      if (this.password == "") this.errors.push("Fill in your password");
 
-        //Check if password has a length longer than 8
+      //Check if password has a length longer than 8
       if (this.password.length < 8 && this.password != "")
         this.errors.push("You're password must be over 8 characters long");
-        //Check if password contains a uppercase letter
+      //Check if password contains a uppercase letter
       if (!this.password.match("[A-Z]") && this.password != "")
-        this.errors.push("You're password must contain 1 uppercase letter");     
+        this.errors.push("You're password must contain 1 uppercase letter");
 
-        //Check if password contains a lowercase letter
+      //Check if password contains a lowercase letter
       if (!this.password.match("[a-z]") && this.password != "")
         this.errors.push("You're password must contain 1 lowercase letter");
 
-        //Check if password contains a special character
+      //Check if password contains a special character
       if (!this.password.match("[?!*:;]") && this.password != "")
-        this.errors.push("You're password must contain a special character: ?, !, *, :, ;");
+        this.errors.push(
+          "You're password must contain a special character: ?, !, *, :, ;"
+        );
 
-        //Check if passwords matchs
-      if(this.password != this.repeatedPassword)
-        this.errors.push("You're repeated password doesn't match")
+      //Check if passwords matchs
+      if (this.password != this.repeatedPassword)
+        this.errors.push("You're repeated password doesn't match");
 
-      if(this.errors.length == 0) this.register(this.username, this.password, this.image)
+      if (this.errors.length == 0)
+        this.register(this.username, this.password, this.image);
     },
     register(username, password, image) {
-        const formData = new FormData()
-        formData.append("file", image)
-        this.$store.dispatch("register", {username: username, password: password, image: image})
+      const formData = new FormData();
+      formData.append("file", image);
+      this.$store.dispatch("register", {
+        username: username,
+        password: password,
+        image: image,
+      });
     },
     onFileChange(e) {
-        const image = e.target.files[0]
-        this.image = image
-    }
+      const image = e.target.files[0];
+      this.image = image;
+    },
   },
 };
 </script>
