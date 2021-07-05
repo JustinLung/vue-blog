1d0ad2f457a67d5eff16b08595607521901920f3
Justin Lung <justin.lung@hva.nl>
2021-07-05 16:33:03 +0200

    changes made

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const multipart = require('connect-multiparty'), multipartyMiddleWare = multipart({uploadDir: './uploads'})
+const multipart = require('connect-multiparty'), multipartyMiddleWare = multipart({ uploadDir: './uploads' })
 const jwt = require('jsonwebtoken')
 const sql = require("./utils/db")
 const app = express()
@@ -14,72 +14,73 @@ app.use(corsConfig)
 
 const port = 3000
 
-app.listen(port, ()=>{ console.log('Listening on port ' + port) })
+app.listen(port, () => { console.log('Listening on port ' + port) })
 
-if(!fs.existsSync('./uploads')) fs.mkdirSync('./uploads')
+if (!fs.existsSync('./uploads')) fs.mkdirSync('./uploads')
 
-app.get('/image/:username/:id', (req, res)=>{
+app.get('/image/:username/:id', (req, res) => {
     const id = req.params.id
     const username = req.params.username
     res.sendFile(`${__dirname}/uploads/${username}/${id}`)
 })
 
-app.post('/auth/login', (req, res)=>{
+app.post('/auth/login', (req, res) => {
     const username = req.body.username
     let password = req.body.password
-
-    password = crypto.createHmac("sha256", "password")
-    .update(password)
-    .digest("hex")
-
-    const hashedUsername = crypto.createHmac('sha1', 'username').update(username).digest('hex')
-
-    const query = sql.query("SELECT username, profile_picture FROM user WHERE username=? AND password=?",
-    [username, password],
-    (err, result)=>{
-        if(err) return res.send(err)
-        if(result.length == 0) return res.status(500).send({error: 'user not found!'})
-        const token = jwt.sign({
-            username: username
-        }, 'secret', {expiresIn: '1h'})
-        result[0].hashedUsername = hashedUsername
-        return res.send({
-            "userInfo": result[0],
-            "accessToken": token
-        })
-    })
-})
-
-app.post('/auth/register', multipartyMiddleWare ,(req, res)=>{
-    const username = req.body.username
-    let password = req.body.password
-    const image = req.files.image.path.split("uploads\\").pop()
-    const date = new Date()
-    const mysqlDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
-    
 
     password = crypto.createHmac("sha256", "password")
         .update(password)
         .digest("hex")
-    
+
+    const hashedUsername = crypto.createHmac('sha1', 'username').update(username).digest('hex')
+
+    const query = sql.query("SELECT username, profile_picture FROM user WHERE username=? AND password=?",
+        [username, password],
+        (err, result) => {
+            if (err) return res.send(err)
+            if (result.length == 0) return res.status(500).send({ error: 'user not found!' })
+            const token = jwt.sign({
+                username: username
+            }, 'secret', { expiresIn: '1h' })
+            result[0].hashedUsername = hashedUsername
+            return res.send({
+                "userInfo": result[0],
+                "accessToken": token
+            })
+        })
+})
+
+app.post('/auth/register', multipartyMiddleWare, (req, res) => {
+    const username = req.body.username
+    let password = req.body.password
+    console.log(req.files)
+    const image = req.files.image.path.split("uploads").pop()
+    const date = new Date()
+    const mysqlDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+
+
+    password = crypto.createHmac("sha256", "password")
+        .update(password)
+        .digest("hex")
+
     const hashedUsername = crypto.createHmac('sha1', 'username').update(username).digest('hex')
 
     const query = sql.query("INSERT INTO `user` VALUES(NULL, ?, ?, ?, ?, ?, 0)",
-    [username, password, image, mysqlDate, mysqlDate],
-    (err, result)=>{
-        if(err){
-            fs.unlinkSync(`./uploads/${image}`)
-            return res.send(err)
-        }
-        if(!fs.existsSync(`./uploads/${hashedUsername}`)) fs.mkdirSync(`./uploads/${hashedUsername}`);
-        fs.renameSync(`./uploads/${image}`, `./uploads/${hashedUsername}/${image}`)
-        return res.send(result)
-    })
+        [username, password, image, mysqlDate, mysqlDate],
+        (err, result) => {
+            if (err) {
+                fs.unlinkSync(`./uploads/${image}`)
+                return res.send(err)
+            }
+            if (!fs.existsSync(`./uploads/${hashedUsername}`)) fs.mkdirSync(`./uploads/${hashedUsername}`);
+            fs.renameSync(`./uploads/${image}`, `./uploads/${hashedUsername}/${image}`)
+            return res.send(result)
+        })
 })
 
-app.get("/test", (req, res)=>{
-    const result = sql.query("SELECT * FROM user", (err, result)=>{
-        if(err) return res.send(err)
+app.get("/test", (req, res) => {
+    const result = sql.query("SELECT * FROM user", (err, result) => {
+        if (err) return res.send(err)
         return res.send(result)
     })
 })
